54c2ca83a63e14730689ee7f658c80195f66fb5e
Chrislorusso07 <chrislorutty@gmail.com>
2023-10-30 17:18:08 -0300

    actualizacion

--- server/src/controllers/getIdDrivers.js
+++ server/src/controllers/getIdDrivers.js
@@ -1,32 +1,22 @@
-const axios = require("axios");
 const { Driver, Teams } = require("../db");
 
-const getIdDrivers = async (id) => {
-  if (id.length < 5) {
-    const response = await axios.get(`http://localhost:5000/drivers/${id}`);
-    const data = response.data;
+const getIdDrivers = async (req, res) => {
+  try {
+    const { idDriver } = req.params;
 
-    const idData = {
-      id: data.id,
-      name: data.name.forename,
-      lastname: data.name.surname,
-      description: data.description,
-      image: data.image.url,
-      nationality: data.nationality,
-      date_of_birth: data.dob,
-      teams: data.teams,
-    };
-
-    return idData;
-  } else {
-    const searchById = await Driver?.findByPk(id, {
+    const response = await Driver.findByPk(idDriver, {
       include: {
         model: Teams,
         attributes: ["name"],
-        through: { attributes: [] },
       },
     });
-    return searchById;
+
+    if (!response) {
+      return res.status(404).json({ error: "Could not find Driver" });
+    }
+    res.status(200).json(response);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
   }
 };
 
